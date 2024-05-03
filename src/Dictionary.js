fb06c1df735f1d581bd041b8b585cf109b2dd070
punSavitri <punshakshi@gmail.com>
2024-05-03 13:35:10 +0100

    created Dictionary.js component and section the form element

---
+++ src/Dictionary.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <section>
+        <form>
+          <label>What word do you want to look up?</label>
+          <input type="serach" autoFocus={true} className="form-control" placeholder="search a word.." required />
+        </form>
+      </section>
+    </div>
+  )
+}
