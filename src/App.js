66392ca7bcb1dfdcea84e6d348fd580c520f55e5
punSavitri <punshakshi@gmail.com>
2024-05-03 14:19:36 +0100

    set defaultsearchkeyword to show keyword  ones page loaded

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>DICTIONARY</h1>
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultSearchKeyword="nature" />
       </main>
       <footer>
         <p>This project was coded by <a href='https://github.com/punSavitri' target='_blank' rel="noreferrer" title='Savitri GitHub repo' >Savitri Pun</a>  and <a href="https://github.com/punSavitri/react-dictionary-project"> is open-sourced on GitHub </a>and <a href="https://react-dictionary-savitri.netlify.app/"> is hosted on Netlify</a></p>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  return (
-    <div className="Dictionary">
-      <section>
-        <form>
-          <label>What word do you want to look up?</label>
-          <input type="serach" autoFocus={true} className="form-control search-input" placeholder="search a word.." required />
-        </form>
-      </section>
-    </div>
-  )
+export default function Dictionary(props) {
+  //console.log(props.defaultSearchKeyword);
+
+  //assigning state variables
+  const [searchKeyword, setSearchKeyword] = useState(props.defaultSearchkeyword);
+  const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setResults(response.data);
+  }
+  function search() {
+    //sheCodes dictionary api documentation
+    let apiKey = "4c3a38t82d64bfo4330f17ff02bfbd97";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchKeyword}&key=${apiKey}`;
+    //console.log(apiUrl);
+    //ajax request to shecodes dictionary api
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handleChange(event) {
+    setSearchKeyword(event.target.value);
+  }
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <label>What word do you want to look up?</label>
+            <input type="serach" autoFocus={true} className="form-control search-input" placeholder="search a word.." defaultValue={props.defaultSearchkeyword} onChange={handleChange} required />
+          </form>
+        </section>
+      </div>
+    )
+  } else {
+    return load();
+  }
+
 }
