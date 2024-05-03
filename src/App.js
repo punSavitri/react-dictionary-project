6cb9dc60bf2b2a617ce7fd48897f9f77a6f414a9
punSavitri <punshakshi@gmail.com>
2024-05-03 13:06:34 +0100

    intial setup, remove unnecessary contents, added favicon on browers tab

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DICTIONARY</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
