374f567a9828507155cd74981b6b964499609a34
punSavitri <punshakshi@gmail.com>
2024-05-03 14:56:12 +0100

    update Results and styles

--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Phonetics from "./Phonetics";
 import "./Results.css";
 
 export default function Results(props) {
   console.log(props.results);
-   if (props.results) {
+  if (props.results) {
     return (
 
       <div className="Results" >
         <section>
           <h2>{props.results.word}</h2>
+          <Phonetics phonetic={props.results.phonetic} />
         </section>
 
       </div>
