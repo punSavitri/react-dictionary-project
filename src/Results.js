40be6c41f73d100bc52f279b95386e689e46745f
punSavitri <punshakshi@gmail.com>
2024-05-03 16:06:37 +0100

    update App styles and Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Phonetics from "./Phonetics";
+import Meaning from "./Meaning";
 import "./Results.css";
 
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
-
       <div className="Results" >
         <section>
           <h2>{props.results.word}</h2>
           <Phonetics phonetic={props.results.phonetic} />
         </section>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <section key={index}>
+              <Meaning meaning={meaning} />
+            </section>);
+        })}
 
       </div>
     )
